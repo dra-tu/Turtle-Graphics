e64143d973d37709879d51e1dcc6c38b31106963
DraTu <RWTreutle@gmail.com>
2025-06-14 19:07:58 +0200

    all words in the text will be highlited

--- src/Controller.java
+++ src/Controller.java
@@ -1,19 +1,25 @@
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Controller extends JPanel implements ActionListener {
+public class Controller extends JPanel implements ActionListener, DocumentListener {
     private static final String START = "Start";
     private static final String CLEAR = "Clear";
 
-    private final JTextArea inputArea;
+    private final JTextPane inputArea;
     private final TurtelPanel turtelPanel;
+    private final StyledDocument doc;
 
     public Controller(int width, int height, TurtelPanel turtelPanel) {
         this.turtelPanel = turtelPanel;
 
-        inputArea = new JTextArea();
+        inputArea = new JTextPane();
+        doc = inputArea.getStyledDocument();
+        doc.addDocumentListener(this);
         inputArea.setBorder(BorderFactory.createLineBorder(Color.RED));
 
         JButton startButton = new JButton(START);
@@ -44,5 +50,52 @@ public class Controller extends JPanel implements ActionListener {
                 inputArea.setText("");
                 break;
         }
+    }
+
+    private void highlight() {
+        String text = inputArea.getText();
+        SwingUtilities.invokeLater(() -> {
+            doc.setCharacterAttributes(0, text.length(), doc.getStyle("default"), true);
+
+            int wordStard = 0;
+            int wordEnd = 0;
+            for (int i = 0; i < text.length(); i++) {
+                wordEnd++;
+
+                if (!Character.isWhitespace(text.charAt(i)) && i != text.length() - 1) continue;
+                if (wordStard == wordEnd) {
+                    wordStard = i + 1;
+                    wordEnd = i + 1;
+                }
+                if (wordEnd != text.length()) {
+                    wordEnd--;
+                } else if (Character.isWhitespace(text.charAt(wordEnd-1))) {
+                    wordEnd--;
+                }
+
+                MutableAttributeSet set = new SimpleAttributeSet();
+                StyleConstants.setForeground(set, Color.RED);
+                StyleConstants.setBackground(set, Color.BLUE);
+
+                doc.setCharacterAttributes(wordStard, wordEnd - wordStard, set, true);
+
+                wordStard = i + 1;
+                wordEnd = i + 1;
+            }
+        });
+    }
+
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        highlight();
+    }
+
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+        highlight();
+    }
+
+    @Override
+    public void changedUpdate(DocumentEvent e) {
     }
 }

4e6ee5af2196f5b8ef1bf99a4ac5925333e04fb9
DraTu <RWTreutle@gmail.com>
2025-06-14 07:31:53 +0200

    no more examble

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,6 @@ public class Main {
 
         TurtelPanel draw = new TurtelPanel(MAX_X, MAX_Y);
         f.add(draw);
-        draw.move(100);
-        draw.rotate("L", 90);
-        draw.move(150);
-        draw.rotate("R", 55);
-        draw.move(250);
 
         Controller controller = new Controller(MAX_X, 50, draw);
         f.add(controller);

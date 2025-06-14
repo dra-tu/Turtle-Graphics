059fa12d5408350016c5096b92d3b1982550c519
DraTu <RWTreutle@gmail.com>
2025-06-14 22:11:33 +0200

    added CALL-IF as KeyWord

--- src/SpecialWords.java
+++ src/SpecialWords.java
@@ -6,25 +6,16 @@ import java.awt.*;
 import java.util.Objects;
 
 public class SpecialWords {
-    private static String[] KeyWords = {
-        "MOVE",
-        "ROTATE",
-        "VAL",
-        "FUN",
-        "END",
-        "CALL"
-    };
+    private static String[] KeyWords = {"MOVE", "ROTATE", "VAL", "CALL-IF", "FUN", "END", "CALL"};
 
-    private static String[] ValModifiers = {
-            "CALC"
-    };
+    private static String[] ValModifiers = {"CALC"};
 
     private final static MutableAttributeSet KEY_WORD_STYLE;
     private final static MutableAttributeSet VAL_MOD_STYLE;
 
     static {
         KEY_WORD_STYLE = new SimpleAttributeSet();
-        StyleConstants.setForeground(KEY_WORD_STYLE , Color.BLUE);
+        StyleConstants.setForeground(KEY_WORD_STYLE, Color.BLUE);
         StyleConstants.setBold(KEY_WORD_STYLE, true);
 
         VAL_MOD_STYLE = new SimpleAttributeSet();
@@ -33,13 +24,13 @@ public class SpecialWords {
     }
 
     public static AttributeSet getStyle(String word) {
-        for (String keyWord: KeyWords) {
+        for (String keyWord : KeyWords) {
             if (Objects.equals(word, keyWord)) {
                 return KEY_WORD_STYLE;
             }
         }
 
-        for (String modifier: ValModifiers) {
+        for (String modifier : ValModifiers) {
             if (word.equals(modifier)) {
                 return VAL_MOD_STYLE;
             }

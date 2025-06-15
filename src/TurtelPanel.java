aeb524e7922ed92e9dddf4682d1f24e4c571edaf
DraTu <RWTreutle@gmail.com>
2025-06-15 07:13:11 +0200

    stuff

--- src/TurtelPanel.java
+++ src/TurtelPanel.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class TurtelPanel extends JPanel {
+    private static final String MAIN_FUN_NAME = "MAIN";
+
     private final ArrayList<Line> lines;
     private final ArrayList<String> errors;
     private final Point turtelPos;
@@ -53,7 +55,7 @@ public class TurtelPanel extends JPanel {
         if (errors.isEmpty()) return;
         g.setColor(Color.RED);
         for (int i = 0; i < errors.size(); i++) {
-            g.drawString(errors.get(i), 0, (i+1) * 20);
+            g.drawString(errors.get(i), 0, (i + 1) * 20);
         }
     }
 
@@ -79,33 +81,39 @@ public class TurtelPanel extends JPanel {
     }
 
     private enum ErrorType {
-        ARG_NUM, NOT_A_NUM, FUN_IN_FUN, END_OUT_OF_FUN, UNKNOWN_FUN,
+        ARG_NUM, NOT_A_NUM, FUN_IN_FUN, END_OUT_OF_FUN, UNKNOWN_FUN, COMP_FALSELY_FORMAT, UNKNOWN_COMP, MAIN_FUN,
     }
-    private void addError(ErrorType e, int line) {
+
+    private void addError(ErrorType e, int line, String funName) {
         switch (e) {
             case ARG_NUM:
-                errors.add("wrong number of args at line " + line);
+                errors.add("wrong number of args at line " + line + " of " + funName);
                 break;
             case NOT_A_NUM:
-                errors.add("Not a number at line " + line);
+                errors.add("Not a number at line " + line + " of " + funName);
                 break;
             case FUN_IN_FUN:
-                errors.add("FUN in Fun at line " + line);
+                errors.add("FUN in Fun at line " + line + " of " + funName);
                 break;
             case END_OUT_OF_FUN:
-                errors.add("END before FUN at line " + line);
+                errors.add("END before FUN at line " + line + " of " + funName);
                 break;
             case UNKNOWN_FUN:
-                errors.add("UNKNOWN FUN at line " + line);
+                errors.add("Unknown FUN at line " + line + " of " + funName);
                 break;
+            case COMP_FALSELY_FORMAT:
+                errors.add("Comparison is falsely formated at line " + line + " of " + funName);
+                break;
+            case UNKNOWN_COMP:
+                errors.add("Unknown comparison at line " + line + " of " + funName);
+                break;
+            case MAIN_FUN:
+                errors.add("FUN with name " + MAIN_FUN_NAME + " is not allowed at line " + line);
         }
     }
 
     private static class NoCompOperatorException extends RuntimeException {}
-    private boolean parseIf(int input) throws NoCompOperatorException {
-
-        // TODO: SPLITTE THE INPUT IN a, b, and op
-
+    private boolean parseIf(int a, int b, String op) throws NoCompOperatorException {
         return switch (op) {
             case "==" -> a == b;
             case "!=" -> a != b;
@@ -152,12 +160,13 @@ public class TurtelPanel extends JPanel {
         HashMap<String, Function> funMap = new HashMap<>();
         HashMap<String, Integer> funValues = new HashMap<>();
 
-        executeCommands(commandList, true, values, funMap, funValues);
+        executeCommands(commandList, true, MAIN_FUN_NAME, values, funMap, funValues);
     }
 
     public void executeCommands(
             String commandList,
             boolean reset,
+            String currentFun,
             HashMap<String, Integer> values,
             HashMap<String, Function> funMap,
             HashMap<String, Integer> funValues
@@ -177,6 +186,11 @@ public class TurtelPanel extends JPanel {
             commands[i] = commands[i].trim();
             String[] command = commands[i].split(" ", 2);
 
+            if (inFun && !command[0].equals("END") && !command[0].equals("FUN")) {
+                funBody.append(commands[i]).append("\n");
+                continue;
+            }
+
             String[] args = (command.length > 1)
                     ? command[1].split("\\|")
                     : new String[0];
@@ -185,16 +199,12 @@ public class TurtelPanel extends JPanel {
                 args[j] = args[j].trim();
             }
 
-            if (inFun && !command[0].equals("END") && !command[0].equals("FUN")) {
-                funBody.append(commands[i]).append("\n");
-                continue;
-            }
-
+            int arg_offset = 0;
             try {
                 switch (command[0]) {
                     case "MOVE":
                         if (args.length != 1) {
-                            addError(ErrorType.ARG_NUM, i);
+                            addError(ErrorType.ARG_NUM, i, currentFun);
                             continue;
                         }
                         int length = parse(values, funValues, args[0]);
@@ -202,7 +212,7 @@ public class TurtelPanel extends JPanel {
                         break;
                     case "ROTATE":
                         if (args.length != 2) {
-                            addError(ErrorType.ARG_NUM, i);
+                            addError(ErrorType.ARG_NUM, i, currentFun);
                             continue;
                         }
                         int angel = parse(values, funValues, args[1]);
@@ -211,15 +221,23 @@ public class TurtelPanel extends JPanel {
 
                     case "VAL":
                         if (args.length != 2) {
-                            addError(ErrorType.ARG_NUM, i);
+                            addError(ErrorType.ARG_NUM, i, currentFun);
                             continue;
                         }
                         values.put(args[0], parse(values, funValues, args[1]));
                         break;
 
                     case "FUN":
+                        if (args.length == 0) {
+                            addError(ErrorType.ARG_NUM, i, funName);
+                            continue;
+                        }
+                        if (args[0].equals(MAIN_FUN_NAME)) {
+                            addError(ErrorType.MAIN_FUN, i, funName);
+                            continue;
+                        }
                         if (inFun) {
-                            addError(ErrorType.FUN_IN_FUN, i);
+                            addError(ErrorType.FUN_IN_FUN, i, currentFun);
                             continue;
                         }
                         inFun = true;
@@ -230,7 +248,7 @@ public class TurtelPanel extends JPanel {
                         break;
                     case "END":
                         if (!inFun) {
-                            addError(ErrorType.END_OUT_OF_FUN, i);
+                            addError(ErrorType.END_OUT_OF_FUN, i, currentFun);
                             continue;
                         }
                         inFun = false;
@@ -238,38 +256,43 @@ public class TurtelPanel extends JPanel {
                         break;
                     case "CALL-IF":
                         if (args.length < 2) {
-                            addError(ErrorType.ARG_NUM, i);
+                            addError(ErrorType.ARG_NUM, i, currentFun);
                             continue;
                         }
-                        boolean result = parseIf(args[0]);
-                        if (result) continue;
-                        String[] newArgs = new String[args.length-3];
-                        System.arraycopy(args, 3, newArgs, 0, newArgs.length);
-                        args = newArgs;
+                        String[] split = args[0].split(" ");
+                        if (split.length != 3) {
+                            addError(ErrorType.COMP_FALSELY_FORMAT, i, currentFun);
+                            continue;
+                        }
+                        int a = parse(values, funValues, split[0]);
+                        int b = parse(values, funValues, split[2]);
+                        boolean result = parseIf(a, b, split[1]);
+
+                        if (!result) continue;
+                        arg_offset = 1;
                     case "CALL":
-                        Function callFun = funMap.get(args[0]);
+                        Function callFun = funMap.get(args[arg_offset]);
                         if (callFun == null) {
-                            addError(ErrorType.UNKNOWN_FUN, i);
+                            addError(ErrorType.UNKNOWN_FUN, i, currentFun);
                             continue;
                         }
-                        if ((args.length - 1) != callFun.getArgsCount()) {
-                            addError(ErrorType.ARG_NUM, i);
+                        if ((args.length - 1 - arg_offset) != callFun.getArgsCount()) {
+                            addError(ErrorType.ARG_NUM, i, currentFun);
                             continue;
                         }
 
                         HashMap<String, Integer> callVals = new HashMap<>();
                         for (int j = 0; j < callFun.getArgsCount(); j++) {
-                            callVals.put(callFun.getArgName(j), parse(values, null, args[j + 1]));
+                            callVals.put(callFun.getArgName(j), parse(values, funValues, args[arg_offset + j + 1]));
                         }
 
-                        executeCommands(callFun.getBody(), false, values, funMap, callVals);
+                        executeCommands(callFun.getBody(), false, args[arg_offset], values, funMap, callVals);
                         break;
                 }
             } catch (NumberFormatException ignored) {
-                addError(ErrorType.NOT_A_NUM, i);
+                addError(ErrorType.NOT_A_NUM, i, funName);
             } catch (NoCompOperatorException ignored) {
-                // TODO
-                System.out.println("NOOOOOOOOO");
+                addError(ErrorType.UNKNOWN_COMP, i, funName);
             }
         }
 
